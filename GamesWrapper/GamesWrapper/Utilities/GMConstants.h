003954ae1bf724261898d0179cdb0b08bd8349df
  Madhusudhan HK <madhusudhanhkmail@gmail.com>
2013-07-07 14:56:16 +0530

    Facebook and Twitter api integration

--- GamesWrapper/GamesWrapper/Utilities/GMConstants.h
+++ GamesWrapper/GamesWrapper/Utilities/GMConstants.h
@@ -9,6 +9,17 @@
 #import <Foundation/Foundation.h>
 
 
+/*
+ *  System Versioning Preprocessor Macros
+ */
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+
  
 #define UIColorFromRGB(rgbValue) [UIColor \
 colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
@@ -20,12 +31,12 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 //Constants for Facebook and Flurry Integration
 
 
-#define Facebook_APP_ID @"164708133700507" // Production Ladbrokes Client Account app key
-//#define Facebook_APP_ID @"279084948895628" // UAT Ladbrokes Client Account app key
+//#define Facebook_APP_ID @"164708133700507" // Production Ladbrokes Client Account app key
+#define Facebook_APP_ID @"279084948895628" // UAT Ladbrokes Client Account app key
 //#define Facebook_APP_ID @"390115054437032" // LBR test account ladbrokesgames@gmail.com , Password : Aditi01*
 
-#define Flurry_API_KEY @"RCDDP9Q8JXFQ76DPQBYS" //  Production  Ladbrokes Client account
-//#define Flurry_API_KEY @"N68WNFJW2P6RNRHVWP9V" //  UAT  Ladbrokes Client account
+//#define Flurry_API_KEY @"RCDDP9Q8JXFQ76DPQBYS" //  Production  Ladbrokes Client account
+#define Flurry_API_KEY @"N68WNFJW2P6RNRHVWP9V" //  UAT  Ladbrokes Client account
 
 
 #define Flurry_Applaunched @"Games Wrapper App Launched"
